770abdc6d46dd20f10f732458f8106d3775d6ef0
dariusdraco <95847901+dariusdraco@users.noreply.github.com>
2022-06-29 11:49:20 +0530

    Add files via upload

---
+++ project console/main.js
@@ -0,0 +1,12 @@
+
+console.log("5");
+console.log("1");
+console.log("25");
+console.log("1");
+var first_name = "Darius";
+var last_name = "Jain"
+console.log(first_name.concat(last_name));
+var a = 1 ;
+var b = 2 ;
+var add = a + b ;
+console.log(add);
